52fe35780befbcbff69c4e3128f7bd2dc7226b8f
BrenoReisSantos <breno.battle@gmail.com>
2022-11-15 03:44:44 -0300

    Monitor da Cisterna

---
+++ src/Monitors/CisternaMonitor.cpp
@@ -0,0 +1,34 @@
+#include "CisternaMonitor.hpp"
+#include "../Clients/CisternaClient.hpp"
+
+CisternaMonitor::CisternaMonitor(float alturaDoSensor, float alturaQuandoCheio, float alturaQuandoVazio, CisternaClient *client)
+{
+    this->client = client;
+
+    this->alturaDoSensor = alturaDoSensor;
+    this->alturaQuandoCheio = alturaQuandoCheio;
+    this->alturaQuandoVazio = alturaQuandoVazio;
+
+    porcentagemDoNivelDeAgua = 100;
+    estado = Cheio;
+
+    porcentagemQuandoCheio = (int)((alturaQuandoCheio / alturaDoSensor) * 100);
+    porcentagemQuandoVazio = (int)((alturaQuandoVazio / alturaDoSensor) * 100);
+
+    trava = false;
+};
+
+void CisternaMonitor::controlaBomba(bool ligar)
+{
+    bombaLigada = ligar;
+};
+
+void CisternaMonitor::gerenciaBomba()
+{
+    if (!client->checaVidaDaCaixa())
+        bombaLigada = false;
+
+    EstadoDoNivelDeAgua estadoDaCaixa = client->checaEstadoDaCaixa();
+    if (estadoDaCaixa == Cheio || estado == Vazio || trava)
+        bombaLigada = false;
+};
